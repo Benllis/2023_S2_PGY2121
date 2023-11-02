7c2d8b2aa3c11fa0f8fdae5eecaea7f33d9dafb2
Benllis <134978644+Benllis@users.noreply.github.com>
2023-11-02 13:22:50 -0300

    Delete EjercicioEmpleados directory

--- EjercicioEmpleados/src/main/java/cl/duoc/ejercicioempleados/Utils/Service/ISecretarioService.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.ejercicioempleados.Utils.Service;
-
-import cl.duoc.ejercicioempleados.DTO.EmpleadoDTO;
-
-public interface ISecretarioService {
-     public EmpleadoDTO editarSecretario(EmpleadoDTO obj, int fax, String despacho);
-}
